759b4d9351cc80f93ef8a14f68eb361be8f9eb5a
Medox36 <giuntinil@bzz.ch>
2022-08-18 22:43:33 +0200

    implemented the main method to start the application

---
+++ src/ch/giuntini/powermodeswitch/PowerModeSwitch.java
@@ -0,0 +1,9 @@
+package ch.giuntini.powermodeswitch;
+
+import java.awt.AWTException;
+
+public class PowerModeSwitch {
+    public static void main(String[] args) throws AWTException {
+        new TrayIco();
+    }
+}
