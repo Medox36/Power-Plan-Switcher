466b497509d807e5ebe732d82f137dacb92c2134
Medox36 <giuntinil@bzz.ch>
2022-08-18 22:39:34 +0200

    implemented the Menu for the system tray icon, to change the power plan, edit the powercfg.txt file or exit the application

---
+++ src/ch/giuntini/powermodeswitch/Menu.java
@@ -0,0 +1,78 @@
+package ch.giuntini.powermodeswitch;
+
+import java.awt.Desktop;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+
+public class Menu extends PopupMenu {
+    private final MenuItem item4;
+    private final String[][] arr;
+
+    public Menu(TrayIco ico) {
+        try {
+            PowerModesReader p = new PowerModesReader();
+            arr = p.readFile();
+            for (String[] strings : arr) {
+                MenuItem item = new MenuItem(strings[1]);
+                item.setActionCommand(strings[0]);
+                item.addActionListener(e -> {
+                    try {
+                        Runtime.getRuntime().exec("powercfg -setactive " + e.getActionCommand());
+                        updateActive();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+                add(item);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        item4 = new MenuItem("");
+        MenuItem item5 = new MenuItem("Open config file...");
+        item5.addActionListener(e -> {
+            try {
+                Desktop.getDesktop().edit(new File("powercfg.txt"));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        MenuItem item6 = new MenuItem("exit");
+        item6.addActionListener(e -> {
+            ico.close();
+        });
+        addSeparator();
+        add(item4);
+        addSeparator();
+        add(item5);
+        addSeparator();
+        add(item6);
+
+        updateActive();
+
+        //reduce memory usage
+        Runtime.getRuntime().gc();
+    }
+
+    public void updateActive() {
+        try {
+            Process p = Runtime.getRuntime().exec("powercfg -getactivescheme");
+            BufferedReader br = p.inputReader();
+            String res = br.readLine();
+            br.close();
+            for (String[] strings : arr) {
+                if (res.contains(strings[0])) {
+                    item4.setLabel("Active: " + strings[1]);
+                    return;
+                }
+            }
+            item4.setLabel("Active: none");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
